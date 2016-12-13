7e7ffa001d495165ca673f3bbcc19995f302612e
Matt Joyce <matt.joyce@symphony.com>
2016-12-13 16:46:28 +0000

    updating setup for pypi

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@
 '''
 
 __author__ = 'Matt Joyce'
-__email__ = 'matt.joyce@symphony.com'
-__copyright__ = "Copyright 2016, Symphony"
+__email__ = 'matt@nycresistor.com'
+__copyright__ = 'Copyright 2016, Symphony Communication Services LLC'
 
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
@@ -26,11 +26,21 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='metronome',
-    version='0.0.4',
-    description='Metronome Bot for Symphony',
-    author='Engineering Services',
+    version='0.0.14',
+    description='metronome diagnostics bot',
+    author='Matt Joyce',
     author_email='matt@nycresistor.com',
-    url='https://github.com/symphonyoss/metronome/',
+    url='https://github.com/symphonyoss/metronome',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='symphony chat diagnostics',
     # install dependencies from requirements.txt
     install_requires=reqs,
     packages=find_packages(),
